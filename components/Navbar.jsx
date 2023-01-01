45fb3b4200655f519cd0c448651b5761a3c13f69
Shaheen <96109270+Shaheen121@users.noreply.github.com>
2023-01-01 23:32:12 +0530

    Third Commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,21 +11,22 @@ const Navbar = () => (
     whileInView="show"
     className={`${styles.xPaddings} py-8 relative`}
   >
-    <div className='absolute w-[50%] inset-0 gradient-01' />
+    <div className="absolute w-[50%] inset-0 gradient-01" />
     <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
       <img
-        src='/search.svg'
-        alt='search'
-        className='w-[24px] h-[24px] object-contain'
+        src="/search.svg"
+        alt="search"
+        className="w-[24px] h-[24px] object-contain"
       />
       <h2
-        className='font-extrabold text-[24px] leading-[30px] text-white'>
+        className="font-extrabold text-[24px] leading-[30px] text-white"
+      >
         METAVERSES
       </h2>
       <img
         src="/menu.svg"
         alt="menu"
-        className='w-[24px] h-[24px] object-contain'
+        className="w-[24px] h-[24px] object-contain"
       />
     </div>
   </motion.nav>
